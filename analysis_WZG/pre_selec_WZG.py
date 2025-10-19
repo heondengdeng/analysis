f0cef109ba1fc636b7ef4ee76e95b43ff7dd34ca
heondengdeng <heondengdeng.1028@gmail.com>
2025-10-20 00:27:57 +0900

    WZG pre_selection 완료.

--- analysis_WZG/pre_selec_WZG.py
+++ analysis_WZG/pre_selec_WZG.py
@@ -64,18 +64,27 @@ for sample in the_list:
 
 events = ak.concatenate(all_events_data)
 
+#zip 으로 mass 만드는 방법 익히기.
+
+
+ELECTRON_MASS = 0.000511
+
 el = ak.zip({
     "pt": events["Electron.PT"],
     "eta": events["Electron.Eta"],
     "phi": events["Electron.Phi"],
     "charge": events["Electron.Charge"],
+    "mass": ak.full_like(events["Electron.PT"], ELECTRON_MASS)
 }, with_name="Momentum4D")
-        
+
+MUON_MASS = 0.1057
+
 mu = ak.zip({
     "pt": events["MuonTight.PT"],
     "eta": events["MuonTight.Eta"],
     "phi": events["MuonTight.Phi"],
     "charge": events["MuonTight.Charge"],
+    "mass": ak.full_like(events["MuonTight.PT"], MUON_MASS)
 }, with_name="Momentum4D")
 
 ph = ak.zip({
@@ -121,19 +130,107 @@ mu_mask = (mu.pt > 15) \
 ph_mask = (ph.pt > 20) \
         & ((abs(ph.eta) < 1.442) | (abs(ph.eta) > 1.566)) & (abs(ph.eta) < 2.5)
 
+jet_mask = (jet.pt > 30) \
+         & (abs(jet.eta)<2.5)
+
 sel_el = el[el_mask]
 sel_mu = mu[mu_mask]
 sel_ph = ph[ph_mask]
+sel_jet = jet[jet_mask]
+
 
 # select 후 variable distribution 확인
 
+# plot_configs = [
+#     (ak.flatten(sel_el.pt),      "Selected Electron pT", 100,  (0, 200), "pT [GeV]", "selected_electron_pt.png"),
+#     (ak.flatten(sel_mu.pt),      "Selected Muon pT",     100,  (0, 200), "pT [GeV]", "selected_muon_pt.png"),
+#     (ak.flatten(sel_ph.pt),      "Selected Photon pT",   100,  (0, 200), "pT [GeV]", "selected_photon_pt.png"),
+#     (ak.flatten(sel_el.eta),     "Selected Electron Eta", 100, (-4, 4),   "Eta",      "selected_electron_eta.png"),
+#     (ak.flatten(sel_mu.eta),     "Selected Muon Eta",    100,  (-4, 4),   "Eta",      "selected_muon_eta.png"),
+#     (ak.flatten(sel_ph.eta),     "Selected Photon Eta",    100,  (-4, 4),   "Eta",      "selected_photon_eta.png")
+# ]
+
+# for data, title, bins, plot_range, xlabel, filename in plot_configs:
+#     plt.figure(figsize=(8, 6))
+#     plt.hist(data, bins=bins, range=plot_range, histtype='step', lw=2)
+#     plt.title(title)
+#     plt.xlabel(xlabel)
+#     plt.ylabel('Number of Objects')
+#     plt.grid(True, linestyle='--', alpha=0.5)
+#     plt.savefig(filename, dpi=300)
+#     plt.close()
+
+
+
+# event selection
+
+#bjet veto
+wp_m_bit = 1 << 1
+is_bjet_m = (sel_jet.btag & wp_m_bit) > 0
+n_bjets = ak.sum(is_bjet_m, axis =1)
+# axis = 1 은 이벤트 안에서 
+# axis = 0 은 여러이벤트의 같은 번째끼리
+bjet_veto_mask = (n_bjets == 0)
+
+# lepton
+
+lep = ak.concatenate([sel_el, sel_mu], axis=1)
+n_lep_mask = (ak.num(lep) == 3)
+
+
+# OSSF
+#fields 옵션 name 부여하는 방법 익히기.
+
+ee_pairs = ak.combinations(sel_el, 2, fields=["i0", "i1"])
+os_e_mask = (ee_pairs.i0.charge * ee_pairs.i1.charge) == -1
+os_ee_pairs = ee_pairs[os_e_mask]
+
+mumu_pairs = ak.combinations(sel_mu, 2, fields=["i0", "i1"])
+os_mu_mask = (mumu_pairs.i0.charge * mumu_pairs.i1.charge) == -1
+os_mumu_pairs = mumu_pairs[os_mu_mask]
+
+ossf_pairs = ak.concatenate([os_ee_pairs, os_mumu_pairs], axis=1)
+
+ossf_mask = (ak.num(ossf_pairs, axis=1) >= 1)
+
+# Z window
+
+z_candidates_p4 = ossf_pairs.i0 + ossf_pairs.i1
+z_mass = z_candidates_p4.mass
+Z_MASS = 91.1876
+z_window = abs(z_mass - Z_MASS) < 15
+z_mass_mask = ak.any(z_window, axis=1)
+
+# MET 
+# firsts 하는 의미 생각하기.
+met_mask = (ak.firsts(met.pt) > 30)
+
+
+event_mask = (n_lep_mask & ossf_mask & z_mass_mask & bjet_veto_mask & met_mask )
+
+# !!주의!! (틀렸던 점)
+# 수 비교는 길이를 비교 해야하니까 ak.num 을 써야함.
+# 리스트 자체를 합하는건 ak.sum 이 아니라 ak.concatenate 를 사용해야함.
+
+print(f'이벤트 컷 전 이벤트 수 : {len(events)}')
+
+
+events = events[event_mask]
+sel_el = sel_el[event_mask]
+sel_mu = sel_mu[event_mask]
+sel_ph = sel_ph[event_mask]
+sel_jet = sel_jet[event_mask]
+met = met[event_mask]
+
+print(f'이벤트 컷 후 이벤트 수 : {len(events)}')
+
 plot_configs = [
-    (ak.flatten(sel_el.pt),      "Selected Electron pT", 100,  (0, 200), "pT [GeV]", "selected_electron_pt.png"),
-    (ak.flatten(sel_mu.pt),      "Selected Muon pT",     100,  (0, 200), "pT [GeV]", "selected_muon_pt.png"),
-    (ak.flatten(sel_ph.pt),      "Selected Photon pT",   100,  (0, 200), "pT [GeV]", "selected_photon_pt.png"),
-    (ak.flatten(sel_el.eta),     "Selected Electron Eta", 100, (-4, 4),   "Eta",      "selected_electron_eta.png"),
-    (ak.flatten(sel_mu.eta),     "Selected Muon Eta",    100,  (-4, 4),   "Eta",      "selected_muon_eta.png"),
-    (ak.flatten(sel_ph.eta),     "Selected Photon Eta",    100,  (-4, 4),   "Eta",      "selected_photon_eta.png")
+    (ak.flatten(sel_el.pt),      "e_Selected Electron pT", 100,  (0, 200), "pT [GeV]", "selected_electron_pt.png"),
+    (ak.flatten(sel_mu.pt),      "e_Selected Muon pT",     100,  (0, 200), "pT [GeV]", "selected_muon_pt.png"),
+    (ak.flatten(sel_ph.pt),      "e_Selected Photon pT",   100,  (0, 200), "pT [GeV]", "selected_photon_pt.png"),
+    (ak.flatten(sel_el.eta),     "e_Selected Electron Eta", 100, (-4, 4),   "Eta",      "selected_electron_eta.png"),
+    (ak.flatten(sel_mu.eta),     "e_Selected Muon Eta",    100,  (-4, 4),   "Eta",      "selected_muon_eta.png"),
+    (ak.flatten(sel_ph.eta),     "e_Selected Photon Eta",    100,  (-4, 4),   "Eta",      "selected_photon_eta.png")
 ]
 
 for data, title, bins, plot_range, xlabel, filename in plot_configs:
